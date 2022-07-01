4dfb2fa820f58dd3ddb64a2a1fd8b72c17e5ca71
vFlicker <flickervladislav@gmail.com>
2022-07-01 22:06:04 +0300

    feat: add TrackCard in Tracks page

--- odyssey-lift-off-part1/client/src/pages/tracks.js
+++ odyssey-lift-off-part1/client/src/pages/tracks.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { Layout } from '../components';
+import { useQuery } from '@apollo/client';
+import { TRACKS } from '../query'
+import { Layout, QueryResult } from '../components';
+import { TrackCard } from '../containers';
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+
+  return (
+    <Layout grid>
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.takesForHome?.map((track) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Tracks;
