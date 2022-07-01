11a115c6255159cc3a1e546b2c5dab68dee9b149
vFlicker <flickervladislav@gmail.com>
2022-07-01 21:17:28 +0300

    feat: add ApolloClient

--- odyssey-lift-off-part1/client/src/index.js
+++ odyssey-lift-off-part1/client/src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
+
 import GlobalStyles from './styles';
 import Pages from './pages';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <Pages />
+    <ApolloProvider client={client}>
+      <GlobalStyles />
+      <Pages />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
