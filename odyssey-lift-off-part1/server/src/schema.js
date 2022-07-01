4421cb7503f38579afcb1600f5641e465561015a
vFlicker <flickervladislav@gmail.com>
2022-07-01 19:17:17 +0300

    feat: add Track schema

---
+++ odyssey-lift-off-part1/server/src/schema.js
@@ -0,0 +1,34 @@
+import { gpl } from 'apollo-server'
+
+const typeDefs = gpl`
+  type Query {
+    "Get tracks array for home page grid"
+    takesForHome: [Track!]!
+  }
+
+  "A track is a group of Modules that teaches about a specific topic"
+  type Track {
+    id: ID!
+    "The track's title"
+    title: String!
+    "The track's main author"
+    author: Author!
+    "The track's main illustration to display in track card or track page detail"
+    thumbnail: String
+    "The track's approximate length to complete, in minutes"
+    length: Int
+    "The number of modules this track contains"
+    modulesCount: Int
+  }
+
+  "Author of a complete Track"
+  type Author {
+    id: ID!
+    "Author's first and last name"
+    name: String!
+    "Author's profile picture url"
+    photo: String
+  }
+`;
+
+export default typeDefs;
