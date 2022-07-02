ee699d3a8349f72304358da07837c4156ff7ce8a
vFlicker <flickervladislav@gmail.com>
2022-07-02 13:43:41 +0300

    feat: update schema
    - add fields to Track
    - add type Module

--- server/src/schema.js
+++ server/src/schema.js
@@ -4,7 +4,9 @@ export const typeDefs = gql`
   type Query {
     "Query to get tracks array for the homepage grid"
     tracksForHome: [Track!]!
+    track(id: ID!): Track
   }
+
   "A track is a group of Modules that teaches about a specific topic"
   type Track {
     id: ID!
@@ -18,7 +20,23 @@ export const typeDefs = gql`
     length: Int
     "The number of modules this track contains"
     modulesCount: Int
+    "The track's complete description, can be in Markdown format"
+    description: String
+    "The number of times a track has been viewed"
+    numberOfViews: Int
+    "The track's complete array of Modules"
+    modules: [Module!]!
   }
+
+  "A Module is a single unit of teaching. Multiple Modules compose a Track"
+  type Module {
+    id: ID!
+    "The Module's title"
+    title: String!
+    "The Module's length in minutes"
+    length: Int
+  }
+
   "Author of a complete Track or a Module"
   type Author {
     id: ID!
@@ -28,3 +46,7 @@ export const typeDefs = gql`
     photo: String
   }
 `;
+
+[Mission!] => [Mission] || null
+[Mission]! => [null] || []
+[Mission!]! => [Mission]
