07179972838668e43addbb7e7315517335320fa9
vFlicker <flickervladislav@gmail.com>
2022-07-04 20:19:17 +0300

    feat: add schemes

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,13 @@
-require('dotenv').config();
+import 'dotenv/config.js';
+import { ApolloServer } from 'apollo-server';
+
+import typeDefs from './schema.js';
+
+const server = new ApolloServer({ typeDefs });
+
+const startApolloServer = async (server) => {
+  const { url } = await server.listen({ port: process.env.PORT || 4000 });
+  console.log(`🚀  Server ready at ${url}`);
+}
+
+startApolloServer(server);
---
+++ server/src/schema.js
@@ -0,0 +1,54 @@
+import { gql } from 'apollo-server';
+
+const typeDefs = gql`
+  type Query {
+    launches: [Launch]!
+    launch(id: ID!): Launch
+    me: User
+  }
+
+  type Mutation {
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
+    cancelTrip(launchId: ID!): TripUpdateResponse!
+    login(email: String): User
+  }
+
+  type TripUpdateResponse {
+    success: Boolean!
+    message: String
+    launches: [Launch]
+  }
+
+  type Launch {
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+  }
+
+  type Rocket {
+    id: ID!
+    name: String
+    type: String
+  }
+
+  type Mission {
+    name: String
+    missionPatch(size: PatchSize): String
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    trips: [Launch]
+    token: String
+  }
+
+  enum PatchSize {
+    SMALL
+    LARGE
+  }
+`;
+
+export default typeDefs;
