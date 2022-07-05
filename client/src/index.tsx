81dca167804667123fe54777377b523588000a00
vFlicker <flickervladislav@gmail.com>
2022-07-05 16:31:19 +0300

    feat(client): add apollo queries

---
+++ client/src/index.tsx
@@ -0,0 +1,27 @@
+import {
+  ApolloClient,
+  NormalizedCacheObject,
+  ApolloProvider
+} from '@apollo/client';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { cache } from './cache';
+import Pages from './pages';
+import injectStyles from './styles';
+
+// Initialize ApolloClient
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  cache,
+  uri: 'http://localhost:4000/graphql',
+});
+
+injectStyles();
+
+// Pass the ApolloClient instance to the ApolloProvider component
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Pages />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
--- client/src/pages/launches.tsx
+++ client/src/pages/launches.tsx
@@ -1,6 +1,14 @@
-import React, { Fragment, useState }  from 'react';
+import React, { Fragment, useState } from 'react';
 import { RouteComponentProps } from '@reach/router';
-import { gql } from '@apollo/client'
+import { gql, useQuery } from '@apollo/client'
+import * as GetLaunchListTypes from './__generated__/GetLaunchList';
+
+import {
+  LaunchTile,
+  Header,
+  Button,
+  Loading
+} from '../components';
 
 export const LAUNCH_TILE_DATA = gql`
   fragment LaunchTile on Launch {
@@ -18,10 +26,70 @@ export const LAUNCH_TILE_DATA = gql`
   }
 `;
 
+export const GET_LAUNCHES = gql`
+  query GetLaunchList($after: String) {
+    launches(after: $after) {
+      # The cursor field indicates the client's current position within
+      # the list of launches. We can execute the query again and provide our most
+      # recent cursor as the value of the $after variable to fetch the next
+      # set of launches in the list.
+      cursor
+      # the field indicates whether there are additional launches beyond
+      # the list returned by the server.
+      hasMore
+      launches {
+        ...LaunchTile
+      }
+    }
+  }
+  ${LAUNCH_TILE_DATA}
+`;
+
 interface LaunchesProps extends RouteComponentProps {}
 
 const Launches: React.FC<LaunchesProps> = () => {
-  return <div />;
+  const {
+    data,
+    loading,
+    error,
+    fetchMore,
+  } = useQuery<
+    GetLaunchListTypes.GetLaunchList,
+    GetLaunchListTypes.GetLaunchListVariables
+  >(GET_LAUNCHES);
+  const [isLoadingMore, setIsLoadingMore] = useState(false); 
+
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR</p>;
+  if (!data) return <p>Not found</p>;
+
+  return (
+    <Fragment>
+      <Header />
+      {data.launches &&
+        data.launches.launches &&
+        data.launches.launches.map((launch: any) => (
+          <LaunchTile key={launch.id} launch={launch} />
+        ))}
+        {data.launches && data.launches.hasMore && (isLoadingMore ? (
+            <Loading />
+          ) : (
+            <Button
+              onClick={async () => {
+                setIsLoadingMore(true);
+                await fetchMore({
+                  variables: {
+                    after: data.launches.cursor
+                  }
+                });
+                setIsLoadingMore(false);
+              }}
+            >
+              Load More
+            </Button>
+          ))}
+    </Fragment>
+  );
 }
 
 export default Launches;
